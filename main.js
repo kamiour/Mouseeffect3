b9f05494ffae230ba47c95685ce2af30c02bf270
Mykhailo Kazakov <mykhailo.kazakov@requestum.com>
2019-11-08 15:11:31 +0200

    new effect

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
-let number = 300;
-let counter = 0;
+let number = 700;
+let radius = 50;
+let initialLeft = [];
+let initialTop = [];
 
 for (let i = 0; i < number; i++) {
-	$("#one").append( '<div id="move-'+i+'" class="move"></div>')
+  $("#one").append( '<div id="move-'+i+'" class="move"></div>');
 }
 
 let elementsArr = $(".move");
 for (let i = 0; i < elementsArr.length; i++) {
-  elementsArr[i].style.transitionDuration = (8 * (Math.random() + 0.1))+'s';
+  /*elementsArr[i].style.transitionDuration = (4 * (Math.random() + 0.2))+'s';*/
+  elementsArr[i].style.transitionDuration = '0.4s';
+  elementsArr[i].style.left = (window.innerWidth * Math.random());
+  elementsArr[i].style.top = (window.innerHeight * Math.random());
+  let diameter = (10 * Math.random() + 5);
+  elementsArr[i].style.width = diameter;
+  elementsArr[i].style.height = diameter;
+  elementsArr[i].style.backgroundColor = 'rgba('+Math.floor((Math.random() * 255) + 1)+','+Math.floor((Math.random() * 255) + 1)+','+Math.floor((Math.random() * 255) + 1)+', 1)';
+  initialLeft[i] = elementsArr[i].style.left;
+  initialTop[i] = elementsArr[i].style.top;
 };
-let sortedArr = elementsArr.sort(function(num1, num2) {
-  return parseFloat(num1.style.transitionDuration) - parseFloat(num2.style.transitionDuration);
-});
-
-function spread() {
-  for (let i = 0; i < elementsArr.length; i++) {
-    elementsArr[i].style.left = (window.innerWidth * Math.random());
-    elementsArr[i].style.top = (window.innerHeight * Math.random());
-    elementsArr[i].style.backgroundColor = 'rgb('+Math.floor((Math.random() * 255) + 1)+','+Math.floor((Math.random() * 255) + 1)+','+Math.floor((Math.random() * 255) + 1)+')';
-  };
-  counter = 1;
-}
-spread();
 
 $(window).mousemove(function(event) {
-  counter = 0;
-  for (let i = 0; i < sortedArr.length; i++) {
-    sortedArr[i].style.left = (event.pageX);
-    sortedArr[i].style.top = (event.pageY);
+  function distance(item) {
+    return Math.pow(Math.pow((parseFloat(item.style.left) - event.pageX), 2) + Math.pow((parseFloat(item.style.top) - event.pageY), 2), 0.5)
   }
-  $(sortedArr[sortedArr.length-1]).one('transitionend webkitTransitionEnd oTransitionEnd', function () {
-    if (counter == 0) {
-      spread();
-    }
-  });
-});
 
+  for (let i = 0; i < elementsArr.length; i++) {
+    elementsArr[i].style.left = initialLeft[i];
+    elementsArr[i].style.top = initialTop[i];
+  }
+  
+  for (let i = 0; i < elementsArr.length; i++) {
+    if (distance(elementsArr[i]) < radius) {
+      elementsArr[i].style.left = (event.pageX);
+      elementsArr[i].style.top = (event.pageY);
+    } else {
+      elementsArr[i].style.left = initialLeft[i];
+      elementsArr[i].style.top = initialTop[i];
+    }
+  }
+});
